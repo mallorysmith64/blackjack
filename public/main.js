ea045066694403d83b44f7b4f77dc12e5071136a
Mallory Smith <mallorysmith6464@gmail.com>
2019-08-08 23:00:23 -0400

    initial

--- public/main.js
+++ public/main.js
@@ -1,7 +1,62 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+et deck = []
+
+//make deck
+const makeCards = () => {
+  let suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+  let ranks = [
+    'Ace',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'Jack',
+    'Queen',
+    'King'
+  ]
+
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < ranks.length; j++) {
+      deck.push(ranks[j] + ' of ' + suits[i])
+    }
+  }
+  console.log(deck)
+  shuffle()
+}
+
+//fisher-yates shuffle
+const shuffle = () => {
+  for (let i = 0; i < deck.length; i++) {
+    const j = Math.floor(Math.random() * i)
+    const cardNow = deck[i]
+    //swap
+    deck[i] = deck[j]
+    deck[j] = cardNow
   }
 }
 
-document.addEventListener('DOMContentLoaded', main)
+//to do
+// const displayCards = () => {
+//   let img = document.querySelector('#show')
+//   let show = deck.pop()
+//   let findCard = './image/' + show + '.png'
+//   img.setAttribute('src', findCard)
+
+//only receive one card
+const getCard = () => {
+  if (deck.length > 0) {
+    const yourCard = deck.pop()
+    console.log(yourCard)
+    const shuffleButton = document.createElement('ul')
+    shuffleButton.textContent = yourCard
+
+    document.querySelector('.randomize').appendChild(shuffleButton)
+  }
+}
+
+document.querySelector('.shuffle-deck').addEventListener('click', getCard)
+document.addEventListener('DOMContentLoaded', makeCards)
